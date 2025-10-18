6ff1ed37dc8565a73a52087a95b41c1fa7286402
Michal Pawlik <admin@michalp.net>
2025-10-18 16:29:47 +0200

    refactor event codes

--- KeyCodes.scala
+++
@@ -1,9 +0,0 @@
-package org.polyvariant.macropad4s
-
-object KeyCodes {
-  val EV_KEY       = 0x01
-  val EV_SYN       = 0x00
-  val SYN_REPORT   = 0x00
-  val KEY_VOLUMEDOWN = 0x72
-  val KEY_VOLUMEUP = 0x73
-}
